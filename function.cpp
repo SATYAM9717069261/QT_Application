8024cd2403bc5f9767fc58bbaf588029e23f8713
SATYAM9717069261 <satyam.mandal.77@gmail.com>
2022-02-12 14:27:37 +0530

    Address of both variable, is diferent

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Function_scope{
+	public:
+		void scope(int add);
+};
+
+void Function_scope::scope(int data){
+	cout<<"2. Address=> "<<&data << "value =>"<<data; 
+}
+
+int main(){
+	int data = 20;
+	cout<<"1. Address=> "<<&data << "value =>"<<data<<endl; 
+	Function_scope user1;
+	user1.scope(10);
+	cout<<"3. Address=> "<<&data << "value =>"<<data<<endl; 
+	return 0;
+}
